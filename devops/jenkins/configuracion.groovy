6ccdcfdcf97e969bfd125d06cfb66bb77e26139c
Felix Edwin Chaina De La Cruz - EVERIS <felixchaina@bcp.com.pe>
2020-12-16 11:49:20 -0500

    configuracion

---
+++ devops/jenkins/configuracion.groovy
@@ -0,0 +1,20 @@
+#!/opt/groovy/bin/groovy
+/*****************************************************/
+
+
+pipeline {
+
+    agent any
+
+    parameters {
+        string(defaultValue: '@DEMO01', description: 'Tags para las pruebas. Ejemplo: @GENERAR_TOKEN', name: 'TAGS')
+        //choice(name: 'Ejecucion', choices: ['Local','Remoto'], description:'Remoto es ejecución en el servidor')
+    }
+
+    stages {
+        stage('checkout') {
+         sh 'ifconfig'
+            sh 'hostname'
+        }
+    }
+}
