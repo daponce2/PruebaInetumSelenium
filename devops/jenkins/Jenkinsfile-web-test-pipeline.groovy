9f81cd9d5f2e398f431e044f6eeaa65f310f8331
Felix Edwin Chaina De La Cruz - EVERIS <felixchaina@bcp.com.pe>
2020-12-16 19:19:36 -0500

    jenkinsfile

--- devops/jenkins/Jenkinsfile-web-test-pipeline.groovy
+++ devops/jenkins/Jenkinsfile-web-test-pipeline.groovy
@@ -12,29 +12,28 @@ def recipients = ''
 def project = "INCT"
 
 try {
-    node('pinctlnxp11-lnx-jenkins-slave')
-            {
-                stage('Preparation') {
-                    //utils.notifyByMail('START',recipients)
-                    checkout scm
-                    utils.prepare()
-                    env.project = "${project}"
-                }
+    node {
+        stage('Preparation') {
+            //utils.notifyByMail('START',recipients)
+            checkout scm
+            utils.prepare()
+            env.project = "${project}"
+        }
 
-                stage('Build') {
-                    utils.buildMaven()
-                }
+        stage('Build') {
+            utils.buildMaven()
+        }
 
-                stage('Test') {
-                    utilsTesting.executeWebFunctionalTest('', params.Environment, params.TAGS)
-                }
+        stage('Test') {
+            utilsTesting.executeWebFunctionalTest('', params.Environment, params.TAGS)
+        }
 
-                stage('Post Execution') {
-                    utils.executePostExecutionTasks()
-                    //utils.notifyByMail('SUCCESS',recipients)
-                }
+        stage('Post Execution') {
+            utils.executePostExecutionTasks()
+            //utils.notifyByMail('SUCCESS',recipients)
+        }
 
-            }
+    }
 } catch (Exception e) {
     node {
         utils.executeOnErrorExecutionTasks()
