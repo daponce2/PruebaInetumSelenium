54cb79fdd5e3654acf55c0c640514e2506965d3b
Felix Edwin Chaina De La Cruz - EVERIS <felixchaina@bcp.com.pe>
2020-12-28 19:01:29 -0500

    log

--- src/test/java/com/bdd/runner/RunnerTest.java
+++ src/test/java/com/bdd/runner/RunnerTest.java
@@ -3,22 +3,31 @@ package com.bdd.runner;
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import web.com.bdd.lib.WebDriverManager;
+import web.com.bdd.util.UtilWeb;
+
+import java.util.logging.Level;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(plugin = {"html:target/build/cucumber-html-report", "pretty:target/build/cucumber-pretty.txt"
-        , "json:target/build/cucumber.json"},
+@CucumberOptions(plugin = {"json:target/build/cucumber.json"},
         features = {"src/test/resources/features"},
         junit = {"--step-notifications"},
         glue = {"com.bdd.web.stepdefinition"},
         tags = {"@DEMO01"}
 )
-public class RunnerTest {
 
+public class RunnerTest {
+    @BeforeClass
+    public static void beforeClass() {
+
+        UtilWeb.logger(RunnerTest.class).log(Level.INFO,"Execute Test");
+    }
 
     @AfterClass
-    public static void afterClass(){
+    public static void afterClass() {
         //Se detiene el driver.
         WebDriverManager.stopWebDriver();
     }
