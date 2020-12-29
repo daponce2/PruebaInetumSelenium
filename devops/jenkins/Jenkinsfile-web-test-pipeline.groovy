0b6af35cce41a450faa4baf51d7ec7a0f78daf1b
Felix Edwin Chaina De La Cruz - EVERIS <felixchaina@bcp.com.pe>
2020-12-29 12:16:45 -0500

    dev

--- devops/jenkins/Jenkinsfile-web-test-pipeline.groovy
+++ devops/jenkins/Jenkinsfile-web-test-pipeline.groovy
@@ -11,28 +11,30 @@ def recipients = ''
 def project = "INCT"
 
 try {
-    node('pinctlnxp12-lnx-jenkins-slave') {
-        stage('Preparation') {
-            //utils.notifyByMail('START',recipients)
-            checkout scm
-            utils.prepare()
-            env.project = "${project}"
-        }
+    //node('pinctlnxp12-lnx-jenkins-slave')
+    node
+            {
+                stage('Preparation') {
+                    //utils.notifyByMail('START',recipients)
+                    checkout scm
+                    utils.prepare()
+                    env.project = "${project}"
+                }
 
-        stage('Build') {
-            utils.buildMaven()
-        }
+                stage('Build') {
+                    utils.buildMaven()
+                }
 
-        stage('Test') {
-            utilsTesting.executeWebFunctionalTest('', params.Environment, params.TAGS)
-        }
+                stage('Test') {
+                    utilsTesting.executeWebFunctionalTest('', params.Environment, params.TAGS)
+                }
 
-        stage('Post Execution') {
-            utils.executePostExecutionTasks()
-            //utils.notifyByMail('SUCCESS',recipients)
-        }
+                stage('Post Execution') {
+                    utils.executePostExecutionTasks()
+                    //utils.notifyByMail('SUCCESS',recipients)
+                }
 
-    }
+            }
 } catch (Exception e) {
     node {
         utils.executeOnErrorExecutionTasks()
